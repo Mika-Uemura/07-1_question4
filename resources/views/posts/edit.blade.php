3a685b6760e8b94c53cf1567fcddc019ba565722
Mika-Uemura <uemuramika2023.s@gmail.com>
2023-04-18 15:49:12 +0900

    08-5ブログ記事編集して保存

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" value="{{ $post->title }}">
+            </div>
+            <div class="content_postbody">
+                <h2>Body</h2>
+                <input type="text" name="post[body]" value="{{ $post->body }}">
+            </div>
+            <input type="submit" value="update"/>
+        </form>
+        <div class="footer">
+            <a href="/post/{{ $post->id }}">戻る</a>
+        </div>
+    </body>
+</html>
